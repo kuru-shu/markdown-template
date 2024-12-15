08bdcd3839f5186cb7dad8b90a9065db3d5c8f07
kuru-shu <kurushu71@gmail.com>
2024-12-15 15:19:33 +0900

    sidepanelに変更

--- src/background.js
+++ src/background.js
@@ -1,3 +1,14 @@
+const welcomePage = 'sidepanels/welcome-sp.html';
+const mainPage = 'sidepanels/main-sp.html';
+
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Markdown Helper extension installed.');
+  chrome.sidePanel.setOptions({ path: welcomePage });
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
+
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const { path } = await chrome.sidePanel.getOptions({ tabId });
+  if (path === welcomePage) {
+    chrome.sidePanel.setOptions({ path: mainPage });
+  }
 });
